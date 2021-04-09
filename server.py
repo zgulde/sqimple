b69a24051c253821ba7ec091bf88fe80efd5cea2
Zach Gulde <zachgulde@gmail.com>
2021-04-09 12:38:30 -0500

    first

---
+++ server.py
@@ -0,0 +1,37 @@
+import flask
+from subprocess import run
+
+app = flask.Flask(__name__, static_url_path="")
+
+DATABASE = ":memory:"
+
+
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
+
+
+@app.route("/query", methods=["POST"])
+def runquery():
+    query = flask.request.get_data(as_text=True)
+    result = run(["sqlite3", "-header", "-html", DATABASE, query], capture_output=True)
+    return flask.jsonify(
+        dict(result=result.stdout.decode(), error=result.stderr.decode())
+    )
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "sqlite_db",
+        nargs="?",
+        help="SQLite database to interact with. Defaults to in-memory db.",
+    )
+    args = parser.parse_args()
+
+    if args.sqlite_db:
+        DATABASE = args.sqlite_db
+
+    app.run(debug=True)
