6f46e2e7adbe2705dc69be1751ac8f8ffa8d646b
Zach Gulde <zachgulde@gmail.com>
2021-04-09 20:55:58 -0500

    Update frontend

---
+++ sqimple/static/main.js
@@ -0,0 +1,53 @@
+const hideErrorOutput = () => {
+  errorOutput.classList.add('hidden')
+}
+
+const showError = error => {
+  errorOutput.classList.remove('hidden')
+  errorOutput.innerText = error
+}
+
+const handleResponse = ({result, error}) => {
+  hideErrorOutput()
+  outputTable.innerHTML = result
+
+  if (error) {
+    showError(error)
+  }
+}
+
+const sendQuery = e => {
+  editor.selectAll()
+  const body = editor.getSelectedText().trim()
+  editor.clearSelection()
+  const options = { method: 'POST', body }
+  fetch('/query', options)
+    .then(response => response.json())
+    .then(handleResponse)
+    .catch(e => showError(`${e.name} ${e.message}`))
+}
+
+const submitButton = document.querySelector('#submit')
+const outputTable = document.querySelector('#output')
+const errorOutput = document.querySelector('#error')
+
+submitButton.addEventListener('click', sendQuery)
+
+const editor = ace.edit('editor')
+editor.session.setMode('ace/mode/sql')
+editor.insert(`WITH RECURSIVE t(id, x, y) AS (
+    SELECT 1, random() % 100, random() % 100
+    UNION
+    SELECT id + 1, random() % 100, random() % 100
+    FROM t WHERE id < 10
+)
+SELECT * FROM t`)
+
+editor.commands.addCommand({
+  name: 'sendQuery',
+  bindKey: {win: 'Ctrl-Enter',  mac: 'Command-Enter'},
+  exec: sendQuery,
+  readOnly: true // false if this command should not apply in readOnly mode
+})
+
+window.onbeforeunload = () => true
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['templates/**/*.html'],
+  purge: ['sqimple/templates/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
